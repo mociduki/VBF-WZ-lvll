73f8f5b1d33262b801d471b2b610fea3a06ed676
Kazuya Mochizuki <52049181+mociduki@users.noreply.github.com>
2020-05-11 14:23:22 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export KERAS_BACKEND=tensorflow
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 . /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh
 . $ATLAS_LOCAL_ROOT_BASE/packageSetups/localSetup.sh "lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt ROOT" \
        "lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt root_numpy" \
