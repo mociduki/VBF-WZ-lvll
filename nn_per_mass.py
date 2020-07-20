0dd2313226e866e4f281d16c1e0017d261b68979
Mochizuki Kazuya <mociduki@atlas14.lps.umontreal.ca>
2020-07-20 14:19:03 -0400

    small update

--- nn_per_mass.py
+++ nn_per_mass.py
@@ -3,5 +3,7 @@ import sys
 
 ROOT.gROOT.ProcessLine(".L nn_per_mass.C+")
 
-if len(sys.argv)>1: ROOT.nn_per_mass(sys.argv[1],sys.argv[2])
+if   len(sys.argv)>3: ROOT.nn_per_mass(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
+elif len(sys.argv)>2: ROOT.nn_per_mass(sys.argv[1],sys.argv[2],sys.argv[3])
+elif len(sys.argv)>1: ROOT.nn_per_mass(sys.argv[1],sys.argv[2])
 else:               ROOT.nn_per_mass()
